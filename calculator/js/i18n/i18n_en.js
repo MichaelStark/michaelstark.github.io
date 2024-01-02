2f2c5abb43cad1f2aab0597dcfbb58fe757f9e48
MichaelStak <stark1912@gmail.com>
2024-01-02 23:35:01 +0100

    update readme

--- calculator/js/i18n/i18n_en.js
+++ calculator/js/i18n/i18n_en.js
@@ -4,22 +4,48 @@ const i18n_en = {
         "orientationIsNotAvailable": "DD Force ON. Device orientation is not available - please use manual mode",
         "magicHistoryIsEnabled": "History ON",
         "magicHistoryIsDisabled": "History OFF",
-        "ddForceIsEnabled": "DD Force ON",        
+        "ddForceIsEnabled": "DD Force ON",
         "ddForceManualIsEnabled": "Manual mode ON",
         "ddForceManualDisabled": "Manual mode OFF",
         "toxicForceIsEnabled": "TOXIC Force ON",
         "history": "History",
         "dd": "Touches left",
         "alert": "Alert",
-        "readme_part_1": `<p style="text-align: center;"><strong>(%) Numerology</strong></p>
+        "readme_part_1": `<div>
+            <div style="text-align: center;"><strong>(⁺&frasl;₋) Remote control</strong></div>
+            </div>
+            <p>Enables/disables remote control mode. With this feature, you can control the magical functions of another similar calculator on another device remotely. To do this, you will need good, stable Internet on both devices. We will call the viewer's phone as <strong>CLIENT</strong> device. What should be done:</p>
+            <ol>
+            <li>First of all, it is important to enable this function once on <strong>YOUR</strong> device (or let's call it <strong>HOST</strong> phone) in order to set a unique permanent identifier for your device. This identifier can only be changed by completely reinstalling the application.</li>
+            <li>Copy the <a onpointerdown="onCopyRCLink()"><strong>GET LINK</strong></a> link which will serve as the connection address for the <strong>CLIENT's</strong> phone. This address contains your unique permanent identifier. Therefore, if you change this <strong>ID</strong>, you will have to d all the following steps again.</li>
+            <li>Use any link shortening service and make the <strong>LINK</strong> shorter</li>
+            <li>You can remember the <strong>LINK</strong> or write an <strong>NFC</strong> tag</li>
+            <li>The label allows you to open a <strong>LINK</strong> with a tap. If there is no label, install the application by entering the <strong>LINK&nbsp;</strong>manually</li>
+            <li>From this moment on, the <strong>CLIENT</strong> will constantly try to find the <strong>HOST</strong> phone at the specified address and connect.</li>
+            <li>The <strong>CLIENT</strong> calculator will work in normal calculator mode, and will secretly enable magic functions upon command from the <strong>HOST</strong> after both phones are connected</li>
+            <li>On the <strong>HOST</strong> phone you need to restart the application and make sure that the remote control function is enabled</li>
+            <li>The <strong>HOST</strong> screen frame will be green if the connection has been established</li>
+            </ol>
+            <p>What do the frame colors mean:</p>
+            <ul>
+            <li>White (flashing) - activation/deactivation of any function in the calculator</li>
+            <li>Yellow (constant) - waiting for <strong>CLIENT</strong> connection</li>
+            <li>Green (constant) - connection is established, everything is fine and you can work</li>
+            <li>Red (constant) - a disconnection has occurred for some reason and the application itself will try to either restore access or reboot to start the connection again. In this case, you need to manually enable the remote control function again and wait for the connection. The <strong>CLIENT's</strong> phone works offline and does not require any action</li>
+            </ul>
+            <p>How to control:</p>
+            <p>The <strong>HOST</strong> phone controls the <strong>CLIENT</strong>. It is also desirable that notifications be allowed. Otherwise, control will not be so convenient. When the frame is green you can enable the functions: History, DD Force and TOXIC Force. Your actions will receive feedback in the form of a notification about the actions taken on the <strong>CLIENT&rsquo;s</strong> phone.</p>`,
+        "readme_part_2": `<p style="text-align: center;"><strong>(%) Numerology</strong></p>
             <p>Allows you to calculate the number of fate in numerology. Enter a <strong>NUMBER</strong> and enable the function. The negative sign and fraction sign will be ignored. After enabling the function, instead of the % sign, 2 digits, for 1 second, separated by a slash, will appear.</p>
             <ul>
             <li>The top one is the number of fate, and is also the remainder of &lt;<strong>NUMBER</strong>/9&gt;. More detailed information can be found in the book Numerologic forces by Nico Heinrich</li>
             <li>The bottom one is the result of the expression &lt;9-the number of fate&gt;. This number is used to show the "Multiple of 9" effect and similar. For example. We ask the viewer to multiply the numbers randomly until we get a number with 7-9 digits. 2 multiplied by 7 multiplied by 5 multiplied, etc. different numbers every time. If the result is 122333444, and the viewer thinks the 4th digit is equal to 3, then the viewer must pronounce the numbers 1,2,2,3,3,4,4,4 (without the intended digit) in any order. By entering these numbers into the calculator and turning on the function, we will see below the number conceived by the viewer. If 9 is displayed, then the viewer could have intended either 9 or 0. To determine, use any phishing method.</li>
-            </ul>`,
-        "readme_part_2": `<p style="text-align: center;"><strong>(+) Toxic force</strong></p>
-            <p>Enter the <strong>NUMBER</strong> and enable the function. After this, the application will wait for the = button to be pressed, and no matter what operations are calculated, the result of the calculation will always be your entered <strong>NUMBER</strong>. The forced number will not be displayed in the history. Instead, there will be a real calculation result.</p>`,
-        "readme_part_3": `<p style="text-align: center;"><strong>(-) DD force</strong></p>
+            </ul>
+            <p>The function does not work in remote control mode for the spectator's phone.</p>`,
+        "readme_part_3": `<p style="text-align: center;"><strong>(+) Toxic force</strong></p>
+            <p>Enter the <strong>NUMBER</strong> and enable the function. After this, the application will wait for the = button to be pressed, and no matter what operations are calculated, the result of the calculation will always be your entered <strong>NUMBER</strong>.</p>
+            <p>In remote control, you just need to do the same on your phone and the function will turn on on the viewer&rsquo;s device.</p>`,
+        "readme_part_4": `<p style="text-align: center;"><strong>(-) DD force</strong></p>
             <p>Or display down force. Designed to force a <strong>NUMBER</strong>&nbsp;by bringing the result of calculations to the force using &ldquo;blind input of a random number&rdquo;.</p>
             <p>Enter the required <strong>NUMBER</strong> for the force and enable the function (Button = disables the function and leads to the required result):</p>
             <ul>
@@ -33,13 +59,15 @@ const i18n_en = {
             <li><strong>manual</strong> - hold down the 0 button for 2 seconds until a white frame appears. This allows you to precisely control when the phone will block input. After turning on, the locking begins from the moment you first tap on the screen and for 10 seconds until the white frame appears again. This mode allows you to force the number by asking the viewer to close their eyes and not turn the phone over. Has priority over automatic mode.</li>
             </ul>
             <p>If the application has access (if not, then after enabling the function the application will notify about it) to the orientation of the device in space, then the mode will be <strong>automatic</strong>. At this moment, clicking on the screen causes a number to appear that is necessary to calculate the forced <strong>NUMBER</strong>. Formula: (any_calculations)&lt;any_operation&gt;X=<strong>NUMBER</strong>. Where X is the number that will be substituted for clicks on the screen. The required number is adapted to the last operation before turning the device over. But it is advisable to use + or -. In the case of the &times; and &divide; operations, the result of the calculation may not be accurate when duplicated on other calculators due to the nature of irrational numbers.</p>
-            <p>If notifications are enabled or X=<strong>NUMBER</strong>, then X will be entered one digit for each touch on the screen while input is blocked. The number of required touches will be displayed in notifications if available. If not, the X will be displayed in its entirety the first time you tap on the screen.&nbsp;Each tap on the screen will trigger an animation of pressing the desired button. This allows to broadcast phone screen to a large screen on stage.</p>`,
-        "readme_part_4": `<p style="text-align: center;"><strong>(-)&nbsp;History peek</strong></p>
+            <p>If notifications are enabled or X=<strong>NUMBER</strong>, then X will be entered one digit for each touch on the screen while input is blocked. The number of required touches will be displayed in notifications if available. If not, the X will be displayed in its entirety the first time you tap on the screen.&nbsp;Each tap on the screen will trigger an animation of pressing the desired button. This allows to broadcast phone screen to a large screen on stage.</p>
+            <p>In remote control mode, you get information about remaining clicks or missing permissions to access the viewer's device orientation. You can also enable "display down mode" for the viewer's phone manually or automatically if you allow access on the viewer's phone in advance if required. Similar to other features, you control when to enable this feature on the viewer's device.</p>`,
+        "readme_part_5": `<p style="text-align: center;"><strong>(-)&nbsp;History peek</strong></p>
             <p>Peeking function or input history. When you first open the app, you'll be asked for permission to access your device's notifications. Depending on the answer,&nbsp;history works in two modes:</p>
             <ul>
             <li><strong>Allowed</strong> - turns on/off the history function and the application will display the history in device notifications after any actions. This allows you to get the history on your watch in real time without touching your phone.</li>
             <li><strong>Not allowed</strong> - leads to a one-time pop-up window with the input history.</li>
-            </ul>`,
+            </ul>
+            <p>In remote control mode, turns on/off the story on the viewer&rsquo;s phone. Information will come to you in the form of notifications.</p>`,
         "readme": `<p style="text-align: center;">Description</p>
             <p style="text-align: justify;">The application is a fully working calculator with the iOS calculator interface except for the engineering mode. Automatically adapt to the locale and language of the operating system. Can be installed as an Android or iOS app, or used as a website in any browser. The application updates automatically. Does not require internet access. If the Internet is too slow and there is a delay in launching / restarting the application, turn off the Internet after the first full launch of the application. If any function does not work, delete the application, clear the site data in the browser and install the application again.</p>
             <p style="text-align: center;">Permissions</p>
@@ -51,13 +79,16 @@ const i18n_en = {
             <ul>
             <ul>
             <li style="text-align: justify;"><strong>(AC/C)</strong><br />Completely restarts the application and clears history.</li>
-            <li style="text-align: justify;"><a href="readme.html?part=1"><strong>(%) Numerology</strong></a></li>
-            <li style="text-align: justify;"><a href="readme.html?part=2"><strong>(+) Toxic force</strong></a></li>
-            <li style="text-align: justify;"><a href="readme.html?part=3"><strong>(-) DD force</strong></a></li>
-            <li style="text-align: justify;"><a href="readme.html?part=4"><strong>(=) History peek</strong></a></li>
+            <li style="text-align: justify;"><a href="readme.html?part=1"><strong>(⁺⁄₋) Remote control</strong></a></li>
+            <li style="text-align: justify;"><a href="readme.html?part=2"><strong>(%) Numerology</strong></a></li>
+            <li style="text-align: justify;"><a href="readme.html?part=3"><strong>(+) Toxic force</strong></a></li>
+            <li style="text-align: justify;"><a href="readme.html?part=4"><strong>(-) DD force</strong></a></li>
+            <li style="text-align: justify;"><a href="readme.html?part=5"><strong>(=) History peek</strong></a></li>
             </ul>
             </ul>
             <p style="text-align: center;"><strong>Changelog</strong></p>
+            <p style="text-align: center;"><strong>1.56</strong></p>
+            <p style="text-align: center;">Added the ability to remotely control the viewer's calculator</p>
             <p style="text-align: center;"><strong>1.55</strong></p>
             <p style="text-align: center;">Improved history display. Now the most recent calculations are at the top of the list. History status on/off are stored in memory. Notifications will constantly appear for every change by the user. Old notifications will be cleared. Only one notification with the current history will be displayed. Improved instructions (slightly). Improved DD Force function. Now you can broadcast your phone screen even when it is upside down. Each press will launch an animation of pressing the desired number to force. And the missing number will be recorded digitally for each click on the screen. Just turn on notifications and receive information on your watch about how many more presses are required to create the desired number for the Force.</p>
             <p style="text-align: center;"><strong>1.51</strong></p>
